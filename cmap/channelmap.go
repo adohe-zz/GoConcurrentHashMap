ca198898282d6c1ef3e872c462fcd875b4c8069f
TonyAdo <coolhzb@163.com>
2014-10-10 16:04:02 +0800

    add the channelmap&slottedmap....

--- cmap/channelmap.go
+++ cmap/channelmap.go
@@ -1,5 +1,30 @@
 package cmap
 
-type ChannelLongConcurrentHashMap struct {
+type OpCode int
 
+const (
+	Get OpCode = iota
+	Put
+)
+type Command struct {
+	operation OpCode
+	key int64
+	value interface {}
+	result chan interface {}
+}
+
+type ChannelLongConcurrentHashMap struct {
+	m map[int64] interface {}
+	c chan Command
+}
+
+func NewChannelLongConcurrentHashMap(bufferSize int) *ChannelLongConcurrentHashMap {
+	m := make(map[int64] interface {})
+	c := make(chan Command, bufferSize)
+
+	go func() {
+
+	}()
+
+	return &ChannelLongConcurrentHashMap{m, c}
 }
--- cmap/slottedmap.go
+++ cmap/slottedmap.go
@@ -5,34 +5,67 @@ import (
 )
 
 type SlottedLongConcurrentHashMap struct {
-	slots int
-	segments []map[int64]interface {}
+	sliceShift uint
+	sliceMask uint
+	slices []map[int64]interface {}
 	locks []sync.RWMutex
 }
 
-func NewSlottedLongConcurrentHashMap(concurrency uint) *SlottedLongConcurrentHashMap {
-	size := 1
-	for size < concurrency {
-		size <<= 1
+func longHash(key int64) int {
+	h := (key ^ (key >>> 32)).(int)
+	h ^= (h >>> 20) ^ (h >>> 12)
+	return h ^ (h >>> 7) ^ (h >>> 4)
+}
+
+func NewSlottedLongConcurrentHashMap(concurrencyLevel, initialCapacity uint) *SlottedLongConcurrentHashMap {
+	sshift := 0
+	ssize := 1
+	for ssize < concurrencyLevel {
+		sshift ++
+		ssize <<= 1
 	}
 
 	m := &SlottedLongConcurrentHashMap{
-		slots: size,
-		segments: make(map[[]int64]interface {}, size),
-		locks: make([]sync.RWMutex, size),
+		sliceShift: 32 - sshift,
+		sliceMask: ssize - 1,
+		slices: make([]map[int64]interface {}, ssize),
+		locks: make([]sync.RWMutex, ssize),
+	}
+
+	c := initialCapacity / ssize
+	if c * ssize < initialCapacity {
+		c ++
+	}
+	cap := 1
+	for cap < c {
+		cap <<= 1
 	}
 
 	for i := range m.segments {
-		m.segments[i] = make(map[int64]interface {})
+		m.segments[i] = make(map[int64]interface {}, cap)
 	}
 
 	return m
 }
 
 func (m *SlottedLongConcurrentHashMap) Put(key int64, value interface {}) {
+	hash := longHash(key)
+	segment := (hash >>> m.sliceShift) & m.sliceMask
 
+	m.locks[segment].Lock()
+	defer m.locks[segment].Unlock()
+
+	m.segments[segment][key] = value
 }
 
 func (m *SlottedLongConcurrentHashMap) Get(key int64) (interface {}, bool) {
+	hash := longHash(key)
+	segment := (hash >>> m.sliceShift) & m.sliceMask
 
+	m.locks[segment].RLock()
+	defer m.locks[segment].RUnlock()
+
+	value, ok := m.segments[segment][key]
+
+	return value, ok
 }
