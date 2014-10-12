b63d4ccfc947927c3bcf448c9527de9c4590fcc2
Tony <coolhzb@163.com>
2014-10-12 13:31:56 +0800

    finish the SlottedLongConcurrentHashMap implementation….

--- cmap/slottedmap.go
+++ cmap/slottedmap.go
@@ -7,7 +7,7 @@ import (
 type SlottedLongConcurrentHashMap struct {
 	sliceShift uint
 	sliceMask uint
-	slices []map[int64]interface {}
+	slices []map[int64]interface{}
 	locks []sync.RWMutex
 }
 
@@ -42,13 +42,13 @@ func NewSlottedLongConcurrentHashMap(concurrencyLevel, initialCapacity uint) *Sl
 	}
 
 	for i := range m.segments {
-		m.segments[i] = make(map[int64]interface {}, cap)
+		m.segments[i] = make(map[int64]interface{}, cap)
 	}
 
 	return m
 }
 
-func (m *SlottedLongConcurrentHashMap) Put(key int64, value interface {}) {
+func (m *SlottedLongConcurrentHashMap) Put(key int64, value interface{}) {
 	hash := longHash(key)
 	segment := (hash >>> m.sliceShift) & m.sliceMask
 
@@ -58,7 +58,7 @@ func (m *SlottedLongConcurrentHashMap) Put(key int64, value interface {}) {
 	m.segments[segment][key] = value
 }
 
-func (m *SlottedLongConcurrentHashMap) Get(key int64) (interface {}, bool) {
+func (m *SlottedLongConcurrentHashMap) Get(key int64) (interface{}, bool) {
 	hash := longHash(key)
 	segment := (hash >>> m.sliceShift) & m.sliceMask
 
@@ -68,4 +68,59 @@ func (m *SlottedLongConcurrentHashMap) Get(key int64) (interface {}, bool) {
 	value, ok := m.segments[segment][key]
 
 	return value, ok
+}
+
+func (m *SlottedLongConcurrentHashMap) Clear() {
+	for i := range m.slices {
+		m.locks[i].Lock()
+		m.slices[i] = make(map[int64]interface{})
+		m.locks[i].Unlock()
+	}	
+}
+
+func (m *SlottedLongConcurrentHashMap) Remove(key int64) {
+	hash := longHash(key)
+	segment := (hash >>> m.sliceShift) & m.sliceMask
+	
+	m.locks[segment].Lock()
+	defer m.locks[segment].Unlock()
+	
+	delete(m.slices[segment], key)
+}
+
+func (m *SlottedLongConcurrentHashMap) Size() int {
+	size := 0
+	for _, s := range m.sizeFactors() {
+		size += s
+	}
+	
+	return size
+}
+
+func (m *SlottedLongConcurrentHashMap) sizeFactors() []int {
+	factors := make([]int, m.sliceMask + 1)
+	
+	for i := range m.slices {
+		m.locks[i].RLock()
+		factors[i] = len(m.slices[i])
+		m.locks[i].RUnlock()
+	}
+	
+	return factors
+}
+
+func (m *SlottedLongConcurrentHashMap) IsEmpty() bool {
+	return m.Size() == 0
+}
+
+func (m *SlottedLongConcurrentHashMap) Contains(key int64) bool {
+	hash := longHash(key)
+	segment := (hash >>> m.sliceShift) & m.sliceMask
+	
+	m.locks[segment].RLock()
+	defer m.locks[segment].RUnlock()
+	
+	_, ok := m.slices[segment][key]
+	
+	return ok
 }
