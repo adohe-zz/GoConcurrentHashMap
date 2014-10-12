5b9d1a76cd85dcf2403e6a7f4a7986ec4e7a3c94
Tony <coolhzb@163.com>
2014-10-12 13:00:28 +0800

    add the LongMap interface implementation….

--- cmap/map.go
+++ cmap/map.go
@@ -1,5 +1,11 @@
 package main
 
-type LongMap interface {
-	
+type LongHashMap interface {
+	Get(key int64) (interface{}, bool)
+	Put(key int64, value interface{})
+	Clear()
+	Remove(key int64)
+	Size() int
+	Contains(key int64) bool
+	IsEmpty() bool	 
 }
