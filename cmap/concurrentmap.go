fbaf106adc2d65627b47dbdebe998d6163759dc7
Tony <coolhzb@163.com>
2014-10-17 01:03:39 +0800

    update constructor according to the algorithm...

--- cmap/concurrentmap.go
+++ cmap/concurrentmap.go
@@ -6,17 +6,20 @@ import (
 )
 
 const (
-	loadFactor = 0.75
+	max_exponent = 32
+	default_load_factor = 0.75
 )
 
 type ConcurrentLongHashMap struct {
+	exponent uint32
 	buckets []unsafe.Pointer
 	size	int64
 	loadFactor	float64		
 }
 
 func NewConcurrentLongHashMap() *ConcurrentLongHashMap {
-	hashMap := &NewConcurrentLongHashMap{}
-	
+	hashMap := &ConcurrentLongHashMap{0, make([]unsafe.Pointer, max_exponent), 0, default_load_factor}
+	b := make([]unsafe.Pointer, 1)
+	hashMap.buckets[0] = unsafe.Pointer(&b)
 	return hashMap
 }
