92794d452f7be09210d9d4c7e2c6de7218d8a60a
TonyAdo <coolhzb@163.com>
2014-10-17 13:59:16 +0800

    ConcurrentLongHashMap add more implementation....

--- cmap/concurrentmap.go
+++ cmap/concurrentmap.go
@@ -10,6 +10,48 @@ const (
 	default_load_factor = 0.75
 )
 
+type Any interface{}
+
+type Equable interface {
+	Equals(Any) bool
+}
+
+type WrapperKey interface {
+	Equable
+	HashCode() int
+}
+
+type Node struct {
+	hashCode int
+	hashKey int
+	key WrapperKey
+	value unsafe.Pointer
+}
+
+func newRealNodeWithHashCode(key WrapperKey, value Any, hashCode int) *Node {
+	return &Node{hashCode, , key, unsafe.Pointer(&value)}
+}
+
+func newRealNode(key WrapperKey, value Any) *Node {
+	return newRealNodeWithHashCode(key, value, key.HashCode())
+}
+
+type LongKey int64
+
+func (key LongKey) HashCode() int {
+	h := int(key ^ (key >> 32))
+	h ^= (h >> 20) ^ (h >> 12)
+	return h ^ (h >> 7) ^ (h >> 4)
+}
+
+func (key LongKey) Equals(any Any) bool {
+	if lk, ok := any.(LongKey); ok {
+		return int64(key) == int64(lk)
+	}	
+	
+	return false
+}
+
 type ConcurrentLongHashMap struct {
 	exponent uint32
 	buckets []unsafe.Pointer
@@ -22,4 +64,12 @@ func NewConcurrentLongHashMap() *ConcurrentLongHashMap {
 	b := make([]unsafe.Pointer, 1)
 	hashMap.buckets[0] = unsafe.Pointer(&b)
 	return hashMap
+}
+
+func (m *ConcurrentLongHashMap) GetByHashCode(hashCode int, key WrapperKey) (any Any, ok bool) {
+	
+}
+
+func (m *ConcurrentLongHashMap) Get(key WrapperKey) (Any, bool) {
+	
 }
